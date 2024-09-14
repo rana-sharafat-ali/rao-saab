98d494685a91e287eb9cee4350d3d5fdd2739d89
Rana Sharafat Ali <raosaifali@gmail.com>
2024-09-14 20:33:38 +0500

    fixed some bugs and viisuals

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -3,13 +3,14 @@ import React from "react";
 export default function Alert(props) {
   return (
     <>
+    <div style={{height:50+'px' , marginTop:50+'px'}}>
      {props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         {props.alert.msg}
       </div>}
 
       
       {/* {props.alert && <h1 className={`text-${props.alert.type}`}>{props.alert.msg}</h1>} */}
-
+      </div>
 
     </>
   );
--- src/Components/usestate-event.js
+++ src/Components/usestate-event.js
@@ -14,7 +14,7 @@ const lcase=()=>{setText(text.toLowerCase()); props.showAlert("Text Transformed"
   
   return (
     <>
-      <div id="textm" className="container mt-5 pt-5">
+      <div id="textm" className="container ">
         <h1 style={{color:props.style==='light'?'black':'white'}}>{props.heading}</h1>
         
         <textarea
@@ -25,13 +25,13 @@ const lcase=()=>{setText(text.toLowerCase()); props.showAlert("Text Transformed"
           onChange={userinput}
           value={text}
           ></textarea>
-        <button onClick={upcase} className="btn btn-outline-success mt-2">Convert to UpperCase</button>
+        <button disabled={text.length===0} onClick={upcase} className="btn btn-outline-success mx-2 my-2">Convert to UpperCase</button>
         
-        <button onClick={lcase} className="btn btn-outline-success mt-2 ml-2">Convert to LowerCase</button>
+        <button disabled={text.length===0}  onClick={lcase} className="btn btn-outline-success mx-2 my-2">Convert to LowerCase</button>
 
-        <button onClick={clr} className="btn btn-outline-danger mt-2 ml-2">Remove</button>
+        <button disabled={text.length===0}  onClick={clr} className="btn btn-outline-danger mx-2 my-2">Remove</button>
 
-          <p style={{color:props.style==='light'?'black':'white' , border:'1px solid white', backgroundColor:props.style==='light'?'white':'#343a40'}} className="p-2 mt-2 "><span className="h3">Summary</span> <br /> Letters {text.length} <br />Words {text.split(' ').length} <br /> Time {0.01*text.split(" ").length} min <br /> <span className="h3"> Preview</span> <br /> {text}</p>
+          <p style={{color:props.style==='light'?'black':'white' , border:'1px solid white', backgroundColor:props.style==='light'?'white':'#343a40'}} className="p-2 mt-2 "><span className="h3">Summary</span> <br /> Letters {text.length} <br />Words {text.split(' ').filter((element)=>{return element.length!==0}).length} <br /> Time {0.01*text.split(" ").filter((element)=>{return element.length!==0}).length} min <br /> <span className="h3"> Preview</span> <br /> {text.length>0?text:"Nothing to Preview"}</p>
       </div>
     </>
   );
